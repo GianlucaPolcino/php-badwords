485bb33746e2c78f744f75e5b6a8a915a3e445df
GianlucaPolcino <gianluca.polcino.gp@gmail.com>
2021-12-17 14:21:45 +0100

    work done

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
+<?php
+
+$string = "Ciao, questa è una stringa in php";
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>First php</title>
 </head>
 <body>
-    
+    <h2><?php echo $string ?> ed è lunga <?php echo strlen($string) ?> lettere.</h2>
+    <h2><?php echo str_replace("php", "***", $string)?> ed è lunga <?php echo strlen(str_replace("php", "***", $string))?> lettere.</h2>
 </body>
 </html>
